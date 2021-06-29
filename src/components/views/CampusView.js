9d0b36bc6a802cb063bc9f5a38aa2643b428e4a1
Melissa <lynch.melissat@gmail.com>
2021-06-29 17:42:02 -0400

    update CampusView.js

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -2,10 +2,6 @@
 
 const CampusView = (props) => {
   const {campus} = props;
-  //waiting for students array to be populated
-  // if (campus.students === undefined){
-  //   return <div>Loading...</div>
-  // }
   return (
     <div>      
       <h1>{campus.name}</h1>
