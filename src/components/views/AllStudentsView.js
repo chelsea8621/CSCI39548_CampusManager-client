1454657745d4873d78e88bb19aebf5ce77819fdb
Melissa <lynch.melissat@gmail.com>
2021-12-02 19:56:11 -0500

    bug fix: button in all students view was linked to wrong route

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -7,7 +7,7 @@ const AllStudentsView = (props) => {
     return (
     <div>
       <p>There are no students.</p>
-      <Link to={`student/new`}>
+      <Link to={`newstudent`}>
         <button>Add New Student</button>
       </Link>
     </div>
